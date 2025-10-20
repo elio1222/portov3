0fa422155145d55a5fa157a80102dba2ce4b0bee
elio1222 <124097037+elio1222@users.noreply.github.com>
2025-10-20 14:58:05 -0400

    styling changes and removed spotify div

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// document.getElementById('login').addEventListener('click', () => {
+//   // Redirect to Netlify function to start OAuth flow
+//   window.location.href = '.netlify/functions/spotifyapi';
+// });
+
+  document.getElementById('song').style.display = 'None';
+  document.querySelector('.spotify').style.display = 'None';
