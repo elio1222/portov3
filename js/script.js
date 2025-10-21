76a7a0ce864c901c3fb8f2088b91d0b2ea6d4944
elio1222 <124097037+elio1222@users.noreply.github.com>
2025-10-20 22:40:58 -0400

    now seriously fixed mobile ios css styling

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@
 
   document.getElementById('song').style.display = 'None';
   document.querySelector('.spotify').style.display = 'None';
+
+  if (document.querySelector('.projects').clientWidth <= 450) {
+    document.getElementById('proj').textContent = "PROJECTS | SWIPE TO EXPLORE ->"
+  }
+  
+  window.addEventListener("load", () => {
+  const intro = document.querySelector(".p-intro");
+  intro.style.display = "none";
+  // Force reflow
+  void intro.offsetWidth;
+  intro.style.display = "";
+});
